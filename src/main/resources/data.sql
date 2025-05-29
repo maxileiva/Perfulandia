4da6a76f5227367a272793063a08059566a2cf89
Gonzalo Viveros <gonzaloviveros@MacBook-Pro-de-Gonzalo.local>
2025-05-29 15:24:24 -0400

    cambios

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO Rol (id_rol, rol, descripcion_rol) VALUES ('1', 'Administrador del Sistema', 'Usuarios con privilegios completos');
+INSERT INTO Rol (id_rol, rol, descripcion_rol) VALUES ('2', 'Gerente de Sucursal', 'Usuarios con privilegios de gerente');
+INSERT INTO Rol (id_rol, rol, descripcion_rol) VALUES ('3', 'Empleado de Ventas', 'Usuarios con privilegios de vendedor');
+INSERT INTO Rol (id_rol, rol, descripcion_rol) VALUES ('4', 'Logística', 'Usuarios con privilegios de encargados de logística');
+INSERT INTO Rol (id_rol, rol, descripcion_rol) VALUES ('5', 'Clientes', 'Usuario/Comprador');
